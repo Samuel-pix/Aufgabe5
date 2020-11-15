05c254935416209140cad370e094188332ecd88c
Samuel-pix <73649741+Samuel-pix@users.noreply.github.com>
2020-11-15 19:40:24 +0100

    Add files via upload

--- praktikum/src/main/java/de/hfu/Hauptklasse.java
+++ praktikum/src/main/java/de/hfu/Hauptklasse.java
@@ -5,10 +5,11 @@ public class Hauptklasse
 {
     public static void main( String[] args )
     {
-        	Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 		System.out.println("Geben Sie ihren Text ein.");
 		String eingabe=scan.nextLine();
 		String ausgabe = eingabe.toUpperCase();
 		System.out.println(ausgabe);
+		scan.close();
     }
 }
